aecb306eb47fa45027d8ceaa54a61a06aa5a721b
gadgelogger <sarayou273142910@gmail.com>
2024-03-16 23:14:40 +0900

    fix:ファイル名をわかりやすいものに変更

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -1,4 +1,4 @@
-import 'package:book_review_app/presentation/pages/book_library.dart';
+import 'package:book_review_app/presentation/pages/book_library_page.dart';
 import 'package:book_review_app/presentation/pages/home_page.dart';
 import 'package:book_review_app/presentation/pages/my_page.dart';
 import 'package:flutter/material.dart';
